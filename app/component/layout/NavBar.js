d3f38466addf38facefedfa478cd1030de983d13
nazmul1487 <nazmul15-1487@diu.edu.bd>
2023-12-22 23:25:44 +0600

    Navabr and footer configuration

---
+++ app/component/layout/NavBar.js
@@ -0,0 +1,101 @@
+'use client'
+import { Fragment, useEffect } from 'react'
+import Image from 'next/image'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
+// import { useGlobalContext } from '@/app/Context/store'
+
+const navigation = [
+  { name: 'ALL PRODUCT', href: '#', current: true },
+  { name: 'Go TO MAIN SITE', href: '#', current: false },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+  }
+
+export default function NavBar() {
+ 
+  return (
+    <Disclosure as="nav" className="bg-gray-950">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-7 w-7" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-7 w-7" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <Link href='/'>
+                    {/* <Image
+                    className="h-8 w-auto" width={250} height={250}
+                    src="/images/banner/Nokia.png"
+                    alt="Your Company"
+                    
+                  /> */}
+                  <p className='text-white'>User Management System</p>
+                  </Link>
+                  
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                        
+                      </a>
+
+                    ))}
+                    {/* {cartdata.map((e,i)=> <p className='bg-gray-900 text-white' key={i}>{e.name} {e.price} {e.quantity}</p>)}
+                        <p className='bg-gray-900 text-white'>UserId: {userId}</p> */}
+                  </div>
+                </div>
+              </div>
+              <div className=" hidden sm:ml-6 sm:block absolute inset-y-0 right-0 items-center pr-2 sm:static sm:inset-auto sm:pr-0">
+                  <p className='text-gray-300 rounded-md px-3 py-2 text-base font-medium'>09638776600</p>
+             </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
