12668a982ca37e9ce9c2865d98634f886eb603f3
nazmul1487 <nazmul15-1487@diu.edu.bd>
2023-12-24 03:02:30 +0600

    user delete functionality

--- app/component/service/Api.js
+++ app/component/service/Api.js
@@ -21,7 +21,7 @@ export const get_users = async () => {
   };
 
   export const get_user_by_id = async (id) => {
-    console.log("IDD", id);
+    // console.log("IDD", id);
     try {
       // const res = await fetch(`http://localhost:8082/users/user/${id}`, {
       //   method: 'GET',
@@ -38,3 +38,14 @@ export const get_users = async () => {
       throw error; // Rethrow the error to handle it in the calling code
     }
   }
+
+
+  export const deleteUserData = async (id) => {
+    try {
+      const deleteUrl = `http://localhost:8082/users/delete/${id}`;
+      const response = await axios.delete(deleteUrl);
+      return response.data; // Assuming the response contains relevant data
+    } catch (error) {
+      throw error;
+    }
+  };
--- app/user/view-users/page.js
+++ app/user/view-users/page.js
@@ -1,9 +1,19 @@
-import { get_users } from '@/app/component/service/Api';
+import { get_users, deleteUserData } from '@/app/component/service/Api';
 import React from 'react'
 import Link from 'next/link';
 
 export default async function ViewAll() {
     const users = await get_users();
+
+    const handleDelete = async (id) => {
+        try {
+          await deleteUserData(id);
+          // Handle success (e.g., update UI, show a success message)
+        } catch (error) {
+          console.error('Error during delete request:', error);
+          // Handle error (e.g., show an error message)
+        }
+      };
     //   loadUsers();
     // const loadUsers = async () => {
     // 	const result = await axios.get(
@@ -105,11 +115,10 @@ export default async function ViewAll() {
                                                 </button>
                                             </td>
                                             <td className="px-1 py-2 border text-red-100 rounded bg-gradient-to-r from-red-500 to-red-800 text-center">
-                                                <button className="btn btn-warning ">
+                                                <button onClick={() => handleDelete(udata.id)}  className="btn btn-warning ">
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
                                                         <path fillRule="evenodd" d="M5 3.25V4H2.75a.75.75 0 0 0 0 1.5h.3l.815 8.15A1.5 1.5 0 0 0 5.357 15h5.285a1.5 1.5 0 0 0 1.493-1.35l.815-8.15h.3a.75.75 0 0 0 0-1.5H11v-.75A2.25 2.25 0 0 0 8.75 1h-1.5A2.25 2.25 0 0 0 5 3.25Zm2.25-.75a.75.75 0 0 0-.75.75V4h3v-.75a.75.75 0 0 0-.75-.75h-1.5ZM6.05 6a.75.75 0 0 1 .787.713l.275 5.5a.75.75 0 0 1-1.498.075l-.275-5.5A.75.75 0 0 1 6.05 6Zm3.9 0a.75.75 0 0 1 .712.787l-.275 5.5a.75.75 0 0 1-1.498-.075l.275-5.5a.75.75 0 0 1 .786-.711Z" clipRule="evenodd" />
                                                     </svg>
-
 
                                                 </button>
                                             </td>
